e21c2412e513dfda4097a9072723fcb4fec00ec5
unknown <dudeomous@gmail.com>
2023-06-23 21:12:34 -0400

    added some code for noteroute still broken

--- Develop/routes/noteRoute.js
+++ Develop/routes/noteRoute.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const e = require('express');
 const noteRoute = require('express').Router();
 
 
@@ -17,8 +18,34 @@ noteRoute.get('/api/notes', (req, res) => {
             res.status(500).json({ error: 'Failed to get notes' });
         } else {
             res.json(JSON.parse(data));
-    }
+        }
+
     });
 });
 
 //post route for adding new notes
+// still need to setup id in the note const 
+noteRoute.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    const note = { title, text, id: tba };
+
+    fs.readFile('../db/db.json', (err, data) => {
+        if (err) {
+            console.log(err)
+            res.status(500).json({ error: 'Failed to get notes' });
+        } else {
+            const notes = JSON.parse(data);
+            notes.push(note);
+
+            fs.writeFile('../db/db.json', JSON.stringify(notes), (err) => {
+                if (err) {
+                    console.log(err)
+                    res.status(500).json({ error: 'Failed to post notes' });
+                } else {
+                    console.log('note added');
+                    res.json(note);
+                }
+            });
+        }
+    });
+});
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
-
+const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 
 
@@ -21,6 +21,53 @@ app.get('/noteRoute', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+//CRUD
+
+
+//get note route
+noteRoute.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
+        if (err) {
+            console.log(err)
+            res.status(500).json({ error: 'Failed to get notes' });
+        } else {
+            res.json(JSON.parse(data));
+        }
+
+    });
+});
+
+//post route for adding new notes
+// still need to setup id in the note const 
+noteRoute.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    const note = { title, text, id: tba };
+
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            console.log(err)
+            res.status(500).json({ error: 'Failed to get notes' });
+        } else {
+            const notes = JSON.parse(data);
+            notes.push(note);
+
+            fs.writeFile('../db/db.json', JSON.stringify(notes), (err) => {
+                if (err) {
+                    console.log(err)
+                    res.status(500).json({ error: 'Failed to post notes' });
+                } else {
+                    console.log('note added');
+                    res.json(note);
+                }
+            });
+        }
+    });
+});
+
+
+
+
 
 // listening
 app.listen(PORT, () => {
